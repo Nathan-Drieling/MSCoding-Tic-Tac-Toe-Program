75fdf63ab0a9cbec8f3223ec9b8317934b0600a1
Nathan Drieling <nathandrieling@gmail.com>
2025-06-10 11:45:06 -0500

    Upload

--- js/app.js
+++ js/app.js
@@ -6,81 +6,81 @@ let msgContainer = document.querySelector('.msg-container');
 let msg = document.querySelector('#msg');
 
 const winPatterns = [
-    [0, 1, 2],
-    [0, 3, 6],
-    [0, 4, 8],
-    [1, 4, 7],
-    [2, 5, 8],
-    [2, 4, 6],
-    [3, 4, 5],
-    [6, 7, 8]
+        [0, 1, 2],
+        [0, 3, 6],
+        [0, 4, 8],
+        [1, 4, 7],
+        [2, 5, 8],
+        [2, 4, 6],
+        [3, 4, 5],
+        [6, 7, 8]
 ];
 
 boxes.forEach((box) => {
-    box.addEventListener('click', function () {
+        box.addEventListener('click', function () {
         if (turnO) {
-            box.innerText = 'O';
-            box.style.color = 'green';
-            turnO = false;
-            box.disabled = true;
-            checkWinner();
+                box.innerText = 'O';
+                box.style.color = 'green';
+                turnO = false;
+                box.disabled = true;
+                checkWinner();
         } else {
-            box.innerText = 'X';
-            box.style.color = 'black';
-            turnO = true;
-            box.disabled = true;
-            checkWinner();
+                box.innerText = 'X';
+                box.style.color = 'black';
+                turnO = true;
+                box.disabled = true;
+                checkWinner();
         }
-    });
+        });
 });
 
 const enableBoxes = () => {
-    for (let box of boxes) {
+        for (let box of boxes) {
         box.disabled = false;
         box.innerText = "";
-    }
+        }
 };
 
 const disableBoxes = () => {
-    for (let box of boxes) {
+        for (let box of boxes) {
         box.disabled = true;
-    }
+        }
 };
 
 const showWinner = (winner) => {
-    msg.innerText = `Congratulations, Winner is ${winner}`;
-    msgContainer.classList.remove('hide');
-    disableBoxes();
+        msg.innerText = `Congratulations, Winner is ${winner}`;
+        msgContainer.classList.remove('hide');
+        disableBoxes();
 };
 
 const checkWinner = () => {
-    let hasWin = false;
-    for (let pattern of winPatterns) {
+        let hasWin = false;
+        for (let pattern of winPatterns) {
         let pos1Val = boxes[pattern[0]].innerText;
         let pos2Val = boxes[pattern[1]].innerText;
         let pos3Val = boxes[pattern[2]].innerText;
 
-        if (pos1Val !== "" && pos2Val!=="" && pos3Val!=="" 
-            && pos1Val === pos2Val && pos2Val === pos3Val) {
-            showWinner(pos1Val);
-            hasWin = true;
-            return;
+        if (pos1Val !== "" && pos2Val!=="" && pos3Val!=="" && 
+                pos1Val === pos2Val && pos2Val === pos3Val) {
+                showWinner(pos1Val);
+                hasWin = true;
+                return;
         }
-    }
+        }
 
-    if (!hasWin) {
+        if (!hasWin) {
         const allBoxes = [...boxes].every((box) => box.innerText !== "");
         if (allBoxes) {
-            msgContainer.classList.remove('hide');
-            msg.innerText = 'Match Drawn';
+                msgContainer.classList.remove('hide');
+                msg.innerText = 'Match Drawn';
         }
-    }
+        }
 };
 
 const resetGame = () => {
-    turnO = true;
-    enableBoxes();
-    msgContainer.classList.add('hide');
+        turnO = true;
+        enableBoxes();
+        msgContainer.classList.add('hide');
 };
 
 newGameBtn.addEventListener('click', resetGame);
